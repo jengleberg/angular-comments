22565b64fd596dba9b07d19ab6010b775014ea2f
jengleberg <jeffengleberg@hotmail.com>
2017-11-02 08:36:38 -0600

    to be completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
+
+// author: String;
+
   comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
-  ];
+	  {text: 'first comment!', author: 'Jeff'},
+      {text: 'nice work!', author: 'Bob'},
+      {text: 'I would also like to congratulate you!', author: 'Jim'}
+  ]
 }
+
+ // addComment(comment)
+
